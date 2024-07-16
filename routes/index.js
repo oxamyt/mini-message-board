170fac1296a5ca495ae1b1dd7d1bc83a4d7e412c
Oxamyt <dentez.danil@gmail.com>
2024-07-16 15:42:28 +0300

    create post request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello world!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+// GET home page
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+// GET new form page
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Messageboard", messages: messages });
+});
+
+// POST new form page
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.authorText,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
